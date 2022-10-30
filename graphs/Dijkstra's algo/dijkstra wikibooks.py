af160ae9e417aef747ecddd2907b3ecf1fb5919a
FGTRK <108999136+FGTRK@users.noreply.github.com>
2022-10-30 14:24:23 +0300

    Update dijkstra wikibooks.py

--- graphs/Dijkstra's algo/dijkstra wikibooks.py
+++ graphs/Dijkstra's algo/dijkstra wikibooks.py
@@ -1,13 +1,12 @@
-#N — количество вершин;
-#S — номер стартовой вершины (отсчитывая от нуля);
-#matrix — матрица смежности исходного графа, где несуществующие рёбра имеют бесконечный вес;
-#В данном случае бесконечность равна 1000000;
+#N — количество вершин
+#S — номер стартовой вершины (отсчитывая от нуля)
+#matrix — матрица смежности исходного графа, где несуществующие рёбра имеют бесконечный вес - float('inf')
 def Dijkstra(N, S, matrix):
 	valid = [True]*N        
-	weight = [1000000]*N
+	weight = [float('inf')]*N
 	weight[S] = 0
 	for i in range(N):
-		min_weight = 1000001
+		min_weight = float('inf')
 		ID_min_weight = -1
 		for j in range(N):
 			if valid[j] and weight[j] < min_weight:
@@ -18,3 +17,16 @@ def Dijkstra(N, S, matrix):
 				weight[z] = weight[ID_min_weight] + matrix[ID_min_weight][z]
 		valid[ID_min_weight] = False
 	return weight
+
+
+#Пример вызова функции с 4-мя вершинами
+"""
+inf = float('inf') #Определяем имя для бесконечности
+mat = [[0,2,4,inf], #Матрица смежности графа
+      [inf,0,4,8],
+      [inf,8,0,1],
+      [8,6,inf,0]]
+
+print(Dijkstra(4,3,mat)) #Вызов функции с аргументами - кол-во вершин 4, поиск маршрута для четвертой вершины, сама матрица
+"""
+#Вывод [8, 6, 10, 0]
