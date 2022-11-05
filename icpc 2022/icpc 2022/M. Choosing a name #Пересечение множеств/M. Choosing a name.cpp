77662233fa0628aa8a4ea40b076266941ca1e7a4
FGTRK <108999136+FGTRK@users.noreply.github.com>
2022-11-05 23:14:08 +0300

    Update M. Choosing a name.cpp

--- icpc 2022/icpc 2022/M. Choosing a name #Пересечение множеств/M. Choosing a name.cpp
+++ icpc 2022/icpc 2022/M. Choosing a name #Пересечение множеств/M. Choosing a name.cpp
@@ -1,53 +1,46 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
-#include <math.h>
-#include <queue>
 #include <set>
- 
+
 using namespace std;
- 
+
 int main() {
 	int n1, n2, m1, m2;
 	cin >> n1 >> n2 >> m1 >> m2;
- 
+
 	set<string> w1; set<string> w2;
 	set<string> b;
- 
+
 	string s;
-	for (int i = 0; i < n1; ++i) {
+	for (int i = 0; i < n1; ++i) { //Вводим имена, которые нравятся Игорю WhiteList
 		cin >> s;
 		w1.insert(s);
 	}
-	for (int i = 0; i < n2; ++i) {
+	for (int i = 0; i < n2; ++i) { //Вводим имена, которые нравятся Ире WhiteList
 		cin >> s;
 		w2.insert(s);
 	}
+	
 	set <string> w;
-	for (auto a : w1)
-		for (auto b : w2)
-			if (a == b) {
-				w.insert(a);
+	for (auto a : w1) //Ищем пересечение множеств n1 и n2
+		for (auto b : w2)            //, т.е. имена, которые нравятся и Игорю и Ире
+			if (a == b) { //Если имена из WhiteList Игоря совпадают с именами из WhiteList Иры
+				w.insert(a);                      //, то добавляем их в общий WhiteList
 				break;
 			}
- 
-	for (int i = 0; i < m1 + m2; ++i) {
+
+	for (int i = 0; i < m1 + m2; ++i) { //Заполняем общий BlackList
 		cin >> s;
 		b.insert(s);
 	}
- 
- 
-	
-	set<string> ans;
+
+	set<string> ans; //Имена, которые есть в WhiteList, но нет в BlackList
 	for (auto e : w) {
-		
-		if (b.count(e) == 0)
-			ans.insert(e);
-	}
- 
-	for (auto e : ans) {
-		cout << e << endl;
+		if (b.count(e) == 0) //Если в BlackList нет имени из WhiteList, то пишем в ответ
+			ans.insert(e); //Имя из WhiteList
 	}
 
+	for (auto e : ans) { //Выводим имена, которые удовлетворяют условию задачи в алфавитном порядке
+		cout << e << endl;
+	}
 }
