d020361a0d251da798a9a5efbc0f03b89f71aa4c
viCodexm <116115112+viCodexm@users.noreply.github.com>
2023-07-05 02:14:21 +0300

    Create trie.h

---
+++ data structures/trees/Prefix tree (trie)/trie.h
@@ -0,0 +1,39 @@
+struct TrieNode {
+    bool isEnd;
+    map<char, TrieNode*> children;
+};
+class Trie {
+    TrieNode root;
+public:
+    Trie() {}
+
+    void insert(string word) {
+        TrieNode* tmp = &root;
+        for (char& c : word) {
+            if (!tmp->children[c])
+                tmp->children[c] = new TrieNode();
+            tmp = tmp->children[c];
+        }
+        tmp->isEnd = true;
+    }
+
+    bool search(string word) {
+        TrieNode* tmp = &root;
+        for (char& c : word) {
+            if (tmp->children.find(c) == tmp->children.end())
+                return false;
+            tmp = tmp->children[c];
+        }
+        return tmp->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* tmp = &root;
+        for (char& c : prefix) {
+            if (tmp->children.find(c) == tmp->children.end())
+                return false;
+            tmp = tmp->children[c];
+        }
+        return true;
+    }
+};
