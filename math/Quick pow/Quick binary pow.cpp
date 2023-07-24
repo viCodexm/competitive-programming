c7b864bd638e61b2b6a823248dfeadce66f06321
viCodexm <116115112+viCodexm@users.noreply.github.com>
2023-07-24 13:34:36 +0300

    Update Quick binary pow.cpp

--- math/Quick pow/Quick binary pow.cpp
+++ math/Quick pow/Quick binary pow.cpp
@@ -8,3 +8,17 @@ int BinaryPow(int x, unsigned int k) {
 	}
 	return res;
 }
+
+double BinPow(double x, int k) {
+	double res = 1.0;
+	bool neg = (k < 0);
+
+	k = abs(k);
+	while (k) {
+		if (k & 1)
+			res *= x;
+		x *= x;
+		k /= 2;
+	}
+	return neg ? (1.0 / res) : res;
+}
