dfeab1239d2cc9adc0cfdf7414e325d2897444bb
FGTRK <108999136+FGTRK@users.noreply.github.com>
2023-07-26 15:38:29 +0300

    Быстрее, короче + PEP8

--- math/Simpson's integration/Simpson's integration.py
+++ math/Simpson's integration/Simpson's integration.py
@@ -1,22 +1,17 @@
-
 import math
+
 
 def f(x):
     return math.sin(x)
 
 
-lower_bound, upper_bound = 1, 7 #нижняя и верхняя границы интеграла
-N = 1000*1000; #количество шагов (уже умноженное на 2)
-integral = 0
+lower_bound, upper_bound = 1, 7  # нижняя и верхняя границы интеграла
+N = 2 * (10 ** 3)  # количество шагов (уже умноженное на 2)
 h = (upper_bound - lower_bound) / N
-for i in range(N + 1):
-    x = lower_bound + h * i
-    if (i == 0 or i == N):
-        integral += f(x)
-    else:
-        if (i&1 == 0):
-            integral += f(x) * 2
-        else: integral += f(x) * 4
-
-integral *= h / 3
+integral = f(lower_bound)
+for i in range(1, N):
+    integral += f(lower_bound + h * i) * 2 * (2 if i & 1 else 1)
+else:
+    integral += f(lower_bound + h * N)
+    integral *= h / 3
 print(integral)
