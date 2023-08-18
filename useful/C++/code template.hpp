d29cda1d6982631f7510737e85358f16ae98d5de
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-08-18 18:04:47 +0300

    Create code template.hpp

---
+++ useful/C++/code template.hpp
@@ -0,0 +1,42 @@
+//#pragma GCC optimize("-Ofast", "unroll-loops")
+//#include <bits/stdc++.h>
+
+#include <unordered_map>
+#include <functional>
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <numeric>
+#include <bitset>
+#include <vector>
+#include <string>
+#include <ranges>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+using namespace std;
+
+#define print(v)    for (auto& e : v) cout << e << " "; cout << "\n";
+#define all(v)      v.begin(), v.end()
+#define ll long long
+#define range(i, a, b) for (int i = a; i < b; ++i)
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<pi> vpi;
+
+int main() {
+    std::cin.tie(0);
+    ios_base::sync_with_stdio(false);
+  
+}
