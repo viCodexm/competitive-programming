271b9f89142cf2dec86671639d6c870dc337d2c8
Dmitry <116115112+viCodexm@users.noreply.github.com>
2023-10-01 11:50:14 +0300

    Update code template.hpp - added read(v) macro

--- useful/C++/code template.hpp
+++ useful/C++/code template.hpp
@@ -12,7 +12,7 @@
 #include <bitset>
 #include <vector>
 #include <string>
-#include <ranges>
+#include <ranges>    // C++23, may not be working on some platforms with lower C++ standart
 #include <queue>
 #include <deque>
 #include <stack>
@@ -23,12 +23,13 @@
 using namespace std;
 
 #define print(v)    for (auto& e : v) cout << e << " "; cout << "\n";
+#define read(v)     for (auto& e : v) cin >> e;
 #define all(v)      v.begin(), v.end()
-#define ll long long
 #define range(i, a, b) for (int i = a; i < b; ++i)
+
+typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-
 typedef vector<char> vc;
 typedef vector<vc> vvc;
 typedef pair<int, int> pi;
@@ -37,6 +38,6 @@ typedef vector<pi> vpi;
 int main() {
     ios_base::sync_with_stdio(0);
     std::cin.tie(0);
-    
-  
+
+    return 0;
 }
